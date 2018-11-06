58a8a68818a8fd7da84ecf1f22760d9c5bbbc371
Malta <niicou96@hotmail.com>
2018-11-06 11:59:37 -0300

    Entrega1.py arreglada

--- entrega1.py
+++ entrega1.py
@@ -5,13 +5,13 @@ from simpleai.search.viewers import WebViewer, BaseViewer
 
 orillas = ((0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(1,0),(2,0),(3,0),(4,0),(5,0),(5,1), (5,2),(5,3),(5,4),(5,5),(4,5),(3,5),(4,5))
 
-state = ((0,0),((2,1),(3,4),(4, 2)),())
+state = ((0,0),((2,1),(3,4),(4,2)),())
         #((POS ROBOT),(POS PEOPLE ),(POS BLOQUES YA PASE))
-salve_pibe = []
+
 class TpProblem(SearchProblem):
 
     def is_goal(self, state):
-        return len(state[1]) == 0
+        return len(state[1]) == 0 and state[0] in orillas
 
     def cost(self,s1,action,s2):
         return 1
@@ -28,7 +28,7 @@ class TpProblem(SearchProblem):
         if col > 0:
             if is_valid((row, col -1), state):
                 actions.append((row, col  -1))
-        if col < 2:
+        if col < 5:
             if is_valid((row, col +1), state):
                 actions.append((row, col + 1))
         if len(state[1]) != 0 and state[0] != (0,0) :
@@ -46,7 +46,7 @@ class TpProblem(SearchProblem):
             aux_state = list(state[1])
             for people in aux_state:
                 if people == (row_action, col_action):
-                    salve_pibe.append(len(salve_pibe)+1)
+
                     del aux_state[count]
                 count += 1
             support_state[1] = tuple(aux_state)
@@ -60,18 +60,22 @@ class TpProblem(SearchProblem):
 
     def heuristic(self,state):
         distances = []
-        people_distance = 0
-        first_people = 0
+        people_distance = []
+        more_distance = 0
         if len(state[1]) > 0:
             for people in state[1]:
                 distances.append(people)
-            first_people = min([manhattan(x, state[0]) for x in distances])
-            people_distance = sum([manhattan(x, state[0]) for x in distances])
+            people_distance = [manhattan(x, state[0]) for x in distances]
+            more_distance = max(people_distance)
+            farther_person = state[1][people_distance.index(more_distance)]
         distances.clear()
         for pos in orillas:
             distances.append(pos)
-        minium_distance = min([manhattan(x, state[0]) for x in distances])
-        return minium_distance + people_distance + first_people
+        if more_distance != 0:
+            minium_distance = min([manhattan(x, farther_person) for x in distances])
+        else:
+            minium_distance = min([manhattan(x, state[0]) for x in distances])
+        return minium_distance + more_distance + len(state[1])
 
 
 def is_valid(pos,state):
@@ -85,11 +89,10 @@ def manhattan(pos1,pos2):
     x2, y2 = pos2
     return abs(x2 - x1) + abs(y2 - y1)
 
-def resolver(metodo_busqueda,posiciones_personas:
-    result = astar(TpProblem(state), graph_search=True, viewer=my_viewer)
-    return result
+#def resolver(metodo_busqueda,posiciones_personas:
+    
+#    return result
 my_viewer = BaseViewer()
-
+result = astar(TpProblem(state), graph_search=True, viewer=my_viewer)
 print(my_viewer.stats)
-print(salve_pibe)
 print(my_viewer.solution_node)
